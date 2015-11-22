34465dad9620593b57673b11a41605bebc2948be
Lucas Hill <kingofallhills@gmail.com>
2015-11-22 14:05:00 -0800

    Adding example map in vitae.

--- src/components/Vitae.js
+++ src/components/Vitae.js
@@ -1,15 +1,28 @@
 import React from 'react';
 
 import Button from 'react-bootstrap/lib/Button';
+import Layers from 'openlayers'
 
 export default React.createClass({
   render() {
     const wellStyles = {maxWidth: 400, margin: '0 auto 10px'};
     return (
-      <div className="well" style={wellStyles}>
-        <Button bsStyle="primary" bsSize="large" block>Block level button</Button>
-        <Button bsSize="large" block>Block level button</Button>
-      </div>
+        <div id="map" className="sidebar-map"></div>
     );
+  },
+
+  componentDidMount() {
+    let map = new Layers.Map({
+      target: 'map',
+      layers: [
+        new Layers.layer.Tile({
+          source: new Layers.source.MapQuest({layer: 'sat'})
+        })
+      ],
+      view: new Layers.View({
+        center: Layers.proj.transform([3.6656, 45.9192], 'EPSG:4326', 'EPSG:3857'),
+        zoom: 6
+      })
+    });
   }
 });
