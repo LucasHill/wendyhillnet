f680c4b2d7c89d9d866deac7bead5dd4731edaeb
Lucas Hill <kingofallhills@gmail.com>
2015-11-22 16:32:49 -0800

    Update artist statement page

--- src/components/Statement.js
+++ src/components/Statement.js
@@ -3,13 +3,28 @@ import React from 'react';
 import Button from 'react-bootstrap/lib/Button';
 
 export default React.createClass({
-  render() {
-    const wellStyles = {maxWidth: 400, margin: '0 auto 10px'};
-    return (
-      <div className="well" style={wellStyles}>
-        <Button bsStyle="primary" bsSize="large" block>Block level button</Button>
-        <Button bsSize="large" block>Block level button</Button>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div>
+                <blockquote className="blockquote-reverse">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
+                    <footer>Wendy Hill</footer>
+                </blockquote>
+
+                <div className="well">
+                    <p>Lorem ipsum dolor sit amet, ut pro facer impedit meliore, nam dicam labore dicunt te. Stet nemore alterum vim cu. Pro et enim autem euripidis, no vix saperet denique concludaturque. Vim expetenda mediocritatem et, nec id consul impedit, nonumes scribentur mel id. Est soluta dicunt accommodare et, sonet melius verterem cu ius. Mutat vocent moderatius eu sed, in sed diam dicam. Vel cu feugait corpora definitiones.
+
+                        Vel ei rebum tacimates incorrupte. Cum te honestatis necessitatibus, dicit alienum ea est. Has alia habeo an, et ius dolorem persecuti. Quis assentior eu eam, inimicus molestiae theophrastus pri at. Prima eripuit vituperatoribus ea eam, ipsum lorem periculis ut per, elit corrumpit no quo. Erant perfecto reformidans in ius, duo inani possit cu.
+
+                        Quis labore appareat quo cu, nostro interpretaris ius ex, ne his commodo accusam. Postea menandri ne has, quo tacimates sadipscing in, nam in solum falli nonumy. Apeirian comprehensam duo ea. Adhuc volutpat sit at.
+
+                        His solum placerat consulatu et, et sit omnes mucius. At quo putent dolorum, ad ius adhuc essent numquam. Est evertitur disputando ea. Mea animal fabellas praesent ei.
+
+                        Ex pro soluta repudiare omittantur, pro ei minim propriae accumsan, eu mundi quodsi expetenda est. Brute tractatos ullamcorper eum cu, pri mandamus electram splendide te. Et dicant tantas repudiare mei, ne has oblique sensibus democritum. Mel ne mucius scribentur, et consul euripidis vituperata per.</p>
+                </div>
+
+            </div>
+
+        );
+    }
 });
