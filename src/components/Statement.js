a5e483d2d5e0b6f2c5b4ebe6f5e64a9b04fe0894
Lucas Hill <kingofallhills@gmail.com>
2016-03-22 00:15:16 -0700

    Add text for real maker statement.

--- src/components/Statement.js
+++ src/components/Statement.js
@@ -1,26 +1,78 @@
 import React from 'react';
 
-import Button from 'react-bootstrap/lib/Button';
-
 export default React.createClass({
     render() {
         return (
             <div>
                 <blockquote className="blockquote-reverse">
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
+                    <p>My fundamental identity is of someone who makes things.</p>
                     <footer>Wendy Hill</footer>
                 </blockquote>
 
                 <div className="well">
-                    <p>Lorem ipsum dolor sit amet, ut pro facer impedit meliore, nam dicam labore dicunt te. Stet nemore alterum vim cu. Pro et enim autem euripidis, no vix saperet denique concludaturque. Vim expetenda mediocritatem et, nec id consul impedit, nonumes scribentur mel id. Est soluta dicunt accommodare et, sonet melius verterem cu ius. Mutat vocent moderatius eu sed, in sed diam dicam. Vel cu feugait corpora definitiones.
+                    <p>Wendy Hill, Maker</p>
 
-                        Vel ei rebum tacimates incorrupte. Cum te honestatis necessitatibus, dicit alienum ea est. Has alia habeo an, et ius dolorem persecuti. Quis assentior eu eam, inimicus molestiae theophrastus pri at. Prima eripuit vituperatoribus ea eam, ipsum lorem periculis ut per, elit corrumpit no quo. Erant perfecto reformidans in ius, duo inani possit cu.
+                    <p>My fundamental identity is of someone who makes things. This goes back to my earliest
 
-                        Quis labore appareat quo cu, nostro interpretaris ius ex, ne his commodo accusam. Postea menandri ne has, quo tacimates sadipscing in, nam in solum falli nonumy. Apeirian comprehensam duo ea. Adhuc volutpat sit at.
+                    memories of drawing with my father and crafting with my mother. My Home Economics
 
-                        His solum placerat consulatu et, et sit omnes mucius. At quo putent dolorum, ad ius adhuc essent numquam. Est evertitur disputando ea. Mea animal fabellas praesent ei.
+                    teachers broke me of my bad self-taught habits while encouraging my creativity; I made
 
-                        Ex pro soluta repudiare omittantur, pro ei minim propriae accumsan, eu mundi quodsi expetenda est. Brute tractatos ullamcorper eum cu, pri mandamus electram splendide te. Et dicant tantas repudiare mei, ne has oblique sensibus democritum. Mel ne mucius scribentur, et consul euripidis vituperata per.</p>
+                    a bread wrapper raincoat, a vinyl bikini bathing suit and a faux-fur coat with bound
+
+                    buttonholes while learning solid sewing skills. When I made my first quilt at age 19, I
+
+                    asked “how hard can it be?”. It’s not hard, but it takes a lifetime to find the quilt within.</p>
+
+                    <p><i>Someone Who Makes Things</i></p>
+
+                    <p>Paper crafts, drawing, painting, 3D pop-ups, cross-stitching, embroidery, weaving,
+
+                    sewing (clothing, stuffed animals, costumes), quilting, whatever—I follow my ideas,
+
+                    learning what I need to learn along the way.</p>
+
+                    <p><i>Encouraging My Creativity</i></p>
+
+                    <p>I credit Mrs. Birkenbuel, my high school Home Economics teacher, for everything I know
+
+                    about sewing. We were allowed a lot of freedom to choose our sewing projects, but we
+
+                    had to learn the skills to do it right (or as right as possible). Make a smocked dress?
+
+                    Sure, but the smocking will be up to par. Use horizontal stripes in a dress with a zipper?
+
+                    Go ahead, but those stripes will line up. It was the perfect combination of learning basic
+
+                    skills plus with the freedom to try anything.</p>
+
+                    <p><i>First Quilt At Age 19</i></p>
+
+                    <p>I lied about being a quilt maker so I could get a booth at the Artifactory in Palo Alto,
+
+                    California. I really did ask myself ‘how hard can it be to learn to quilt?’. I’ve been
+
+                    pursuing the answers ever since. I stocked my booth with some baby quilts and cute
+
+                    quilted accessories. One day, a Stanford University student walked in and ordered a
+
+                    custom quilt: a replica of the floor of the Taj Mahal, 120” by 130”, in black and white
+
+                    corduroy. There was no one to tell me I couldn’t or shouldn’t do it. But I did have the
+
+                    voice of Mrs. Birkenbuel whispering you can do it in my mind.</p>
+
+                    <p><i>It Takes a Lifetime</i></p>
+
+                    <p>Athletes talk about The Zone but makers experience this too. When immersed in the
+
+                    creative process, deep in the optimal zone of concentration and discovery, time stops
+
+                    moving. We ask what if questions and handle the materials until something appears out
+
+                    of nothing. It’s not hard to start making things, but for many of us, it’s impossible to
+
+                    stop. We must keep searching for that quilt-or some other object-within.</p>
                 </div>
 
             </div>
